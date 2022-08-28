79e8c1416a0ba40f1a4712386c339b345c20d1dc
LFabre <lucas.p.fabre@gmail.com>
2022-08-28 20:52:56 +0100

    feat: added eslint configurations

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  ignorePatterns: [
+    'node_modules/*',
+    '.next/*',
+    '.out/*',
+    'build/*',
+    '!.prettierrc.js',
+  ],
+  rules: {
+    'eol-last': ['error', 'always'],
+    'no-trailing-spaces': 'error',
+    'no-multi-spaces': 'warn',
+    'quotes': [2, 'single', { 'avoidEscape': true }],
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        ignoreRestSiblings: true,
+        varsIgnorePattern: '_*'
+      },
+    ],
+    'react/jsx-key': 'off',
+    'react/no-unescaped-entities': 'off'
+  },
+};
